3b2bda5085d44131b51d8b0dcc8eadd5b0406b70
toyjack <toyjack@gmail.com>
2023-01-16 10:46:14 +0900

    changed: ssr to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: netlify()
 });
